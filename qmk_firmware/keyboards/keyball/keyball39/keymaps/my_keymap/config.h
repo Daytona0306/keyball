68296c3d137f427160d7de59c179bff10d5270a2
Daytona0306 <y.o.19910306@gmail.com>
2024-05-13 17:19:14 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/my_keymap/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/my_keymap/config.h
@@ -39,7 +39,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_THRESHOLD 200
 #define AUTO_MOUSE_DEFAULT_LAYER 4
 
-#define TAPPING_TERM  200
+#define TAPPING_TERM  2000
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
 
