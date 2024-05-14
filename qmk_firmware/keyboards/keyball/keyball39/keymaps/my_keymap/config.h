15290b7a66936c9d2774307bc016c46900ba4374
Daytona0306 <y.o.19910306@gmail.com>
2024-05-14 10:58:57 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/my_keymap/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/my_keymap/config.h
@@ -36,11 +36,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAP_CODE_DELAY 6
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_THRESHOLD 20000
+//#define AUTO_MOUSE_THRESHOLD 20000
 #define AUTO_MOUSE_DEFAULT_LAYER 4
 
-#define TAPPING_TERM  200
+#define TAPPING_TERM  180
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
 
-#define KEYBALL_CPI_DEFAULT 750 // マウス速度 (default: 500)
+//#define KEYBALL_CPI_DEFAULT 750 // マウス速度 (default: 500)
