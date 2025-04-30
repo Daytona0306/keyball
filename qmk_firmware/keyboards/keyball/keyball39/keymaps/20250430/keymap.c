a6bd0e9b9fd3092de638c2fedf6564d3d6ca60c9
Daytona0306 <y.o.19910306@gmail.com>
2025-05-01 00:31:06 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/20250430/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/20250430/keymap.c
@@ -102,7 +102,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 void keyball_on_apply_motion_to_mouse_move(keyball_motion_t *m, report_mouse_t *r, bool is_left) {
     uint8_t layer = get_highest_layer(layer_state);
     int sensitivity_threshold = 2; // 動き始めの閾値。必要なら調整。
-    int key_tap_divisor = 1.5; // 閾値を超えた移動量を割って、タップ回数を決める係数。調整で感触が変わる。
+    int key_tap_divisor = 3; // 閾値を超えた移動量を割って、タップ回数を決める係数。調整で感触が変わる。
     int max_taps = 1; // 一回の処理で生成する最大タップ数。無限ループ防止用。
     int tap_delay_ms = 10; // 連続タップの間の遅延(ms)。必要なら調整。
 
@@ -218,7 +218,7 @@ void keyball_on_apply_motion_to_mouse_scroll(keyball_motion_t *m, report_mouse_t
         // 高速 -> 低いインデックス (1) -> 小さい除数 -> 速いスクロール
 
         // シグモイド関数の項 (速度に対して 0 から 1 へ変化, speed = 8.0 で 0.5)
-        float sigmoid_term = 1.0f / (1.0f + expf(-0.03f * (speed - 80.0f)));
+        float sigmoid_term = 1.0f / (1.0f + expf(-0.04f * (speed - 80.0f)));
 
         // sigmoid_term (0..1) を 除数インデックスの範囲 (1..7) にマッピング
         // 高速側 (sigmoid_term が 1 に近い) を インデックス 1 に、
