cdabd3cfc6452f5784aa9ac17b752081c8fa5e10
Daytona0306 <y.o.19910306@gmail.com>
2025-04-30 17:52:32 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/20250430/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/20250430/keymap.c
@@ -1,69 +1,30 @@
-
 /*
 Copyright 2022 @Yowkees
 Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
+it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 2 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
-
 #include "quantum.h"
 #include "pointing_device.h"
+// Keyball ライブラリのヘッダーをインクルード
+// Keyball の lib ディレクトリからのパスを指定
 #include "lib/keyball/keyball.h"
 
-// clang-format off
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  // keymap for default (VIA)
-  [0] = LAYOUT_universal(
-    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
-    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  ,
-    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
-    KC_LCTL  , KC_LGUI  , KC_LALT  ,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),LT(1,KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT
-  ),
-
-  [1] = LAYOUT_universal(
-    S(KC_Q)   , S(KC_W) , S(KC_E)  , S(KC_R)  , S(KC_T)  ,                            S(KC_Y)  , S(KC_U)  , S(KC_I)  , S(KC_O)  , S(KC_P)  ,
-    S(KC_A)   , S(KC_S) , S(KC_D)  , S(KC_F)  , S(KC_G)  ,                            S(KC_H)  , S(KC_J)  , S(KC_K)  , S(KC_L)  , KC_QUOT  ,
-    S(KC_Z)   , S(KC_X) , S(KC_C)  , S(KC_V)  , S(KC_B)  ,                            S(KC_N)  , S(KC_M)  ,S(KC_COMM), S(KC_DOT),S(KC_SLSH),
-    KC_LCTL   , KC_LGUI , KC_LALT  , _______  , _______  , _______  ,      _______  , _______  , _______  , KC_RALT  , KC_RGUI  , KC_RSFT
-  ),
-
-  [2] = LAYOUT_universal(
-    _______   , KC_7    , KC_8     , KC_9     , _______  ,                            _______  , KC_LEFT  , KC_UP    , KC_RGHT  , _______  ,
-    _______   , KC_4    , KC_5     , KC_6     ,S(KC_SCLN),                            KC_PGUP  , KC_BTN1  , KC_DOWN  , KC_BTN2  , KC_BTN3  ,
-    _______   , KC_1    , KC_2     , KC_3     ,S(KC_MINS),                            KC_PGDN  , _______  , _______  , _______  , _______  ,
-    _______   , KC_0    , KC_DOT   , _______  , _______  , _______  ,      KC_DEL   , _______  , _______  , _______  , _______  , _______
-  ),
-
-  [3] = LAYOUT_universal(
-    RGB_TOG  , _______  , _______  , _______  ,  _______  ,                           RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  ,  SCRL_DVI ,                           RGB_M_K  , RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  ,  SCRL_DVD ,                           CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
-    QK_BOOT    , KBC_RST  , _______  , _______  ,  _______  , _______  ,     _______  , _______  , _______  , _______  , KBC_RST  , QK_BOOT
-  ),
-};
-// clang-format on
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    // Auto enable scroll mode when the highest layer is 3
-     keyball_set_scroll_mode(get_highest_layer(state) == 3);
- 
-    return state;
-}
-
-// Keyball デフォルト処理で使用されているヘルパー関数をコピー
+// Keyball デフォルト処理で使用されている静的ヘルパー関数をコピー
+// これらは keyball.c から持ってきています
 static int16_t add16(int16_t a, int16_t b) {
     int16_t r = a + b;
     if (a >= 0 && b >= 0 && r < 0) {
@@ -85,6 +46,7 @@ static inline int8_t clip2int8(int16_t v) {
 }
 
 // Keyball デフォルト処理で使用されている速度調整関数をコピー
+// これも keyball.c から持ってきています
 static void adjust_mouse_speed(keyball_motion_t *m) {
     int16_t movement_size = abs(m->x) + abs(m->y);
 
@@ -110,7 +72,60 @@ static void adjust_mouse_speed(keyball_motion_t *m) {
 }
 
 
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  // keymap for default (VIA)
+  [0] = LAYOUT_universal(
+    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
+    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  ,
+    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
+    KC_LCTL  , KC_LGUI  , KC_LALT  ,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),MO(5)    ,KC_RALT,KC_RGUI, KC_RSFT // <<== 例: MO(5) を右下のキーに割り当て
+  ),
+
+  [1] = LAYOUT_universal(
+    S(KC_Q)   , S(KC_W) , S(KC_E)  , S(KC_R)  , S(KC_T)  ,                            S(KC_Y)  , S(KC_U)  , S(KC_I)  , S(KC_O)  , S(KC_P)  ,
+    S(KC_A)   , S(KC_S) , S(KC_D)  , S(KC_F)  , S(KC_G)  ,                            S(KC_H)  , S(KC_J)  , S(KC_K)  , S(KC_L)  , KC_QUOT  ,
+    S(KC_Z)   , S(KC_X) , S(KC_C)  , S(KC_V)  , S(KC_B)  ,                            S(KC_N)  , S(KC_M)  ,S(KC_COMM), S(KC_DOT),S(KC_SLSH),
+    KC_LCTL   , KC_LGUI , KC_LALT  , _______  , _______  , _______  ,      _______  , _______  , _______  , KC_RALT  , KC_RGUI  , KC_RSFT
+  ),
+
+  [2] = LAYOUT_universal(
+    _______   , KC_7    , KC_8     , KC_9     , _______  ,                            _______  , KC_LEFT  , KC_UP    , KC_RGHT  , _______  ,
+    _______   , KC_4    , KC_5     , KC_6     ,S(KC_SCLN),                            KC_PGUP  , KC_BTN1  , KC_DOWN  , KC_BTN2  , KC_BTN3  ,
+    _______   , KC_1    , KC_2     , KC_3     ,S(KC_MINS),                            KC_PGDN  , _______  , _______  , _______  , _______  ,
+    _______   , KC_0    , KC_DOT   , _______  , _______  , _______  ,      KC_DEL   , _______  , _______  , _______  , _______  , _______
+  ),
+
+  [3] = LAYOUT_universal(
+    RGB_TOG  , _______  , _______  , _______  ,  _______  ,                           RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  ,  SCRL_DVI ,                           RGB_M_K  , RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  ,  SCRL_DVD ,                           CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
+    QK_BOOT    , KBC_RST  , _______  , _______  ,  _______  , _______  ,     _______  , _______  , _______  , _______  , KBC_RST  , QK_BOOT
+  ),
+  // <<== レイヤー5の定義をここに追加
+  // このレイヤーに切り替えるためのキー（例: MO(5) など）を
+  // 他のレイヤー（例: レイヤー0）に設定する必要があります。
+  [5] = LAYOUT_universal( // レイヤー5 (トラックボール矢印キー)
+    _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  ),
+};
+// clang-format on
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    // Auto enable scroll mode when the highest layer is 3
+    // この行はKeyball独自のスクロールモードに必要とのことなので、そのままにします。
+    keyball_set_scroll_mode(get_highest_layer(state) == 3);
+
+    // (OLED表示など、他のレイヤー切り替え時の処理が必要であればここに追加)
+
+    return state;
+}
+
 // Keyball の keyball_on_apply_motion_to_mouse_move 弱い関数をオーバーライド
+// レイヤー5での矢印キー変換と、その他のレイヤーでのデフォルト移動処理を実装
 void keyball_on_apply_motion_to_mouse_move(keyball_motion_t *m, report_mouse_t *r, bool is_left) {
     // 現在のレイヤーを取得します。
     uint8_t layer = get_highest_layer(layer_state);
@@ -139,25 +154,26 @@ void keyball_on_apply_motion_to_mouse_move(keyball_motion_t *m, report_mouse_t *
 
         // しきい値を超えた移動量があるかチェックし、矢印キーをタップ
         if (abs(original_delta_x) > sensitivity_threshold || abs(original_delta_y) > sensitivity_threshold) {
-            // レイヤー5での矢印キーマッピング
-            // Keyballのデフォルト処理がX/Yを入れ替えるため、ここではその逆を行います。
+            // レイヤー5での矢印キーマッピング (ユーザーの観測に合わせた軸入れ替え)
             // 元のX移動量(horizontal)をVertical Arrow Keysに、元のY移動量(vertical)をHorizontal Arrow Keysにマップします。
             if (abs(original_delta_x) > abs(original_delta_y)) { // 支配的な元のX移動 (Physical Left/Right)
                 // これをVertical Arrow Keys (UP/DOWN) にマップ
-                 if (original_delta_x < -sensitivity_threshold) { tap_code(KC_UP); } // 元X- (Left) -> KC_UP (ユーザー観測ではUP)
-                else if (original_delta_x > sensitivity_threshold) { tap_code(KC_DOWN); } // 元X+ (Right) -> KC_DOWN (ユーザー観測ではDOWN)
+                 if (original_delta_x < -sensitivity_threshold) { tap_code(KC_UP); } // 元X- (Left) -> KC_UP
+                else if (original_delta_x > sensitivity_threshold) { tap_code(KC_DOWN); } // 元X+ (Right) -> KC_DOWN
 
             } else { // 支配的な元のY移動 (Physical Up/Down)
                 // これをHorizontal Arrow Keys (LEFT/RIGHT) にマップ
-                 if (original_delta_y < -sensitivity_threshold) { tap_code(KC_LEFT); } // 元Y- (Up) -> KC_LEFT (ユーザー観測ではLEFT)
-                else if (original_delta_y > sensitivity_threshold) { tap_code(KC_RIGHT); } // 元Y+ (Down) -> KC_RIGHT (ユーザー観測ではRIGHT)
+                 if (original_delta_y < -sensitivity_threshold) { tap_code(KC_LEFT); } // 元Y- (Up) -> KC_LEFT
+                else if (original_delta_y > sensitivity_threshold) { tap_code(KC_RIGHT); } // 元Y+ (Down) -> KC_RIGHT
             }
         }
     } else {
         // レイヤーが5以外の場合、Keyball のデフォルトの移動処理を実行します。
         // このコードは keyball.c の keyball_on_apply_motion_to_mouse_move のデフォルト実装からコピーしています。
         // これにより、Layer 5 以外のレイヤーで通常のマウス移動が機能するようになります。
-        // adjust_mouse_speed(m); // adjust_mouse_speed は keyball.c の static 関数なので直接呼べません。速度調整が必要ならロジックをコピーするか別の方法で。ここでは軸マッピングのみコピー。
+
+        // <<== adjust_mouse_speed の呼び出しを追加
+        adjust_mouse_speed(m);
 
         #if KEYBALL_MODEL == 61 || KEYBALL_MODEL == 39 || KEYBALL_MODEL == 147 || KEYBALL_MODEL == 44
             // Keyball 39/61/147/44 のデフォルト軸マッピング (YをXに、XをYに) と反転
@@ -185,13 +201,15 @@ void keyball_on_apply_motion_to_mouse_move(keyball_motion_t *m, report_mouse_t *
     // r の内容を使ってレポートを送信します。
 }
 
+// Keyball の keyball_on_apply_motion_to_mouse_scroll 弱い関数をオーバーライド
 // スクロールに加速度を適用
 void keyball_on_apply_motion_to_mouse_scroll(keyball_motion_t *m, report_mouse_t *r, bool is_left) {
     // 現在のレイヤーを取得します。（厳密にはスクロールモード有効時のみ呼ばれる想定ですが安全のため）
-    uint8_t layer = get_highest_layer(layer_state);
+    uint8_t layer = get_highest_layer(layer_state); // この変数は現在使用されていませんが、今後使う可能性を考慮し残しています
 
     // レイヤー3（スクロールモード）の場合のみ処理を行います。
     // Layer 3 で keyball.scroll_mode が true に設定されている前提です。
+    // この関数は Keyball 内部で keyball.scroll_mode が true の場合に呼ばれます。
 
     // <<== スクロールにも adjust_mouse_speed を適用
     adjust_mouse_speed(m);
@@ -227,13 +245,14 @@ void keyball_on_apply_motion_to_mouse_scroll(keyball_motion_t *m, report_mouse_t
     // 関数から戻ると、Keyball の calling code が r の内容を使ってレポートを送信します。
 }
 
+
 #ifdef OLED_ENABLE
 
-#    include "lib/oledkit/oledkit.h"
+#    include "lib/oledkit/oledkit.h"
 
 void oledkit_render_info_user(void) {
-    keyball_oled_render_keyinfo();
-    keyball_oled_render_ballinfo();
-    keyball_oled_render_layerinfo();
+    keyball_oled_render_keyinfo();
+    keyball_oled_render_ballinfo();
+    keyball_oled_render_layerinfo();
 }
 #endif
