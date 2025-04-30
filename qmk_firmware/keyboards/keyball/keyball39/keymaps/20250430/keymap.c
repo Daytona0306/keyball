33a8ea6808858eb68d42fb0b197fc6ee3a921ae9
Daytona0306 <y.o.19910306@gmail.com>
2025-04-30 19:35:32 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/20250430/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/20250430/keymap.c
@@ -9,11 +9,11 @@ Free Software Foundation, either version 2 of the License, or
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
@@ -74,64 +74,64 @@ static void adjust_mouse_speed(keyball_motion_t *m) {
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  // Layer 0: Base Layer (from PDF Cheat Sheet)
-  [0] = LAYOUT_universal(
+  // Layer 0: Base Layer (from PDF Cheat Sheet)
+  [0] = LAYOUT_universal(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
     KC_LCTL, KC_LGUI, KC_LALT, MO(4),   LT(2,KC_SPC),LT(3,KC_LNG1),  KC_BSPC, LT(2,KC_ENT),MO(5), KC_RALT, KC_RGUI, KC_RSFT
-  ),
+  ),
 
-  // Layer 1: Shift Layer (from PDF Cheat Sheet) - Assumed to be Shift + Base Layer
-  [1] = LAYOUT_universal(
+  // Layer 1: Shift Layer (from PDF Cheat Sheet) - Assumed to be Shift + Base Layer
+  [1] = LAYOUT_universal(
     S(KC_Q), S(KC_W), S(KC_E), S(KC_R), S(KC_T),                     S(KC_Y), S(KC_U), S(KC_I), S(KC_O), S(KC_P),
     S(KC_A), S(KC_S), S(KC_D), S(KC_F), S(KC_G),                     S(KC_H), S(KC_J), S(KC_K), S(KC_L), KC_QUOT, // PDF shows ' for KC_QUOT
     S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B),                     S(KC_N), S(KC_M), S(KC_COMM),S(KC_DOT),S(KC_SLSH),
     KC_LCTL, KC_LGUI, KC_LALT, _______, _______, _______,           _______, _______, _______, KC_RALT, KC_RGUI, KC_RSFT
-  ),
+  ),
 
-  // Layer 2: Number/Symbol/Mouse Layer (from PDF Cheat Sheet - Image 5 interpretation)
-  [2] = LAYOUT_universal(
+  // Layer 2: Number/Symbol/Mouse Layer (from PDF Cheat Sheet - Image 5 interpretation)
+  [2] = LAYOUT_universal(
     S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),                     KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_SLASH, M(3),   // Image 5 R0: S(1)-S(5) | KP7-KP/, Macro 4
     S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0),                     KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_ASTERISK,M(4),   // Image 5 R1: S(6)-S(0) | KP4-KP*, Macro 5
     S(KC_MINS),S(KC_EQUAL),S(KC_BSLS), M(1),    M(0),                KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_MINUS, M(2),   // Image 5 R2: S(-)=,~,\, M1,M0 | KP1-KP-, Macro 3
     KC_CAPS, LCTL(KC_6),LCTL(KC_2), KC_LSFT, KC_LALT, KC_TAB,        KC_LANG1,S(KC_LANG2),KC_DEL,  KC_RALT, KC_RGUI, KC_RSFT // Image 5 R3: Mods, Lang, Del, Right Mods
-  ),
+  ),
 
-  // Layer 3: Function/RGB/CPI/Scroll Div Layer (Prioritizing user's previous functional L3 and PDF text over potentially inconsistent images 6/7)
-  [3] = LAYOUT_universal(
-    RGB_TOG  , _______  , _______  , _______  ,  _______  ,                           RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  ,  SCRL_DVI ,                           RGB_M_K  , RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  ,  SCRL_DVD ,                           CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
-    QK_BOOT    , KBC_RST  , _______  , _______  ,  _______  , _______  ,     _______  , _______  , _______  , _______  , KBC_RST  , QK_BOOT
-  ),
+  // Layer 3: Function/RGB/CPI/Scroll Div Layer (Prioritizing user's previous functional L3 and PDF text over potentially inconsistent images 6/7)
+  [3] = LAYOUT_universal(
+    RGB_TOG  , _______  , _______  , _______  ,  _______  ,                           RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  ,  SCRL_DVI ,                           RGB_M_K  , RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  ,  SCRL_DVD ,                           CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
+    QK_BOOT    , KBC_RST  , _______  , _______  ,  _______  , _______  ,     _______  , _______  , _______  , _______  , KBC_RST  , QK_BOOT
+  ),
 
   // Layer 4: Num Pad / Audio / Function Keys (from PDF Cheat Sheet - Image 8 & 9 interpretation)
-  [4] = LAYOUT_universal(
+  [4] = LAYOUT_universal(
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,                       TO(0),   KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_SLASH,
     LCTL(KC_A),KC_F5,   KC_F6,   KC_F7,   KC_F8,                       KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_ASTERISK,_______,
     KC_LSFT, KC_F9,   KC_F10,  KC_F11,  KC_F12,                      KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_MINUS, _______,
     TO(4),   _______, _______, _______, _______, _______,           KC_KP_0, KC_KP_DOT, KC_KP_PLUS,KC_KP_ENTER,KC_DEL,  _______
-  ),
+  ),
 
   // Layer 5: Trackball Arrow Keys / Navigation / Edit (from PDF Cheat Sheet - Image 10 interpretation)
-  [5] = LAYOUT_universal( // レイヤー5 (トラックボール矢印キー)
+  [5] = LAYOUT_universal( // レイヤー5 (トラックボール矢印キー)
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                     _______, _______, _______, _______, _______,
     KC_LEFT, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, // Trackball movement handled by code override
     KC_DEL,  KC_END,  KC_DOWN, KC_PGDN, _______,                     _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______ // Bottom row seems unassigned in PDF L5
-  ),
+  ),
 };
 // clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    // Auto enable scroll mode when the highest layer is 3
-    // この行はKeyball独自のスクロールモードに必要とのことなので、そのままにします。
-    keyball_set_scroll_mode(get_highest_layer(state) == 3);
+    // Auto enable scroll mode when the highest layer is 3
+    // この行はKeyball独自のスクロールモードに必要とのことなので、そのままにします。
+    keyball_set_scroll_mode(get_highest_layer(state) == 3);
 
-    // (OLED表示など、他のレイヤー切り替え時の処理が必要であればここに追加)
+    // (OLED表示など、他のレイヤー切り替え時の処理が必要であればここに追加)
 
-    return state;
+    return state;
 }
 
 // Keyball の keyball_on_apply_motion_to_mouse_move 弱い関数をオーバーライド
@@ -243,7 +243,7 @@ void keyball_on_apply_motion_to_mouse_scroll(keyball_motion_t *m, report_mouse_t
     r->h = clip2int8(x);
     r->v = clip2int8(y);
 #else
-#    error("unknown Keyball model")
+#    error("unknown Keyball model")
 #endif
 
     // KeyballのデフォルトにはScroll snappingのロジックも含まれますが、
@@ -258,11 +258,11 @@ void keyball_on_apply_motion_to_mouse_scroll(keyball_motion_t *m, report_mouse_t
 
 #ifdef OLED_ENABLE
 
-#    include "lib/oledkit/oledkit.h"
+#    include "lib/oledkit/oledkit.h"
 
 void oledkit_render_info_user(void) {
-    keyball_oled_render_keyinfo();
-    keyball_oled_render_ballinfo();
-    keyball_oled_render_layerinfo();
+    keyball_oled_render_keyinfo();
+    keyball_oled_render_ballinfo();
+    keyball_oled_render_layerinfo();
 }
 #endif
