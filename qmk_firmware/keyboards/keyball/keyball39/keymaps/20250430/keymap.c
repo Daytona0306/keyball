0b03179de239448a9f90f4f36c8b770f3c42bdf1
Daytona0306 <y.o.19910306@gmail.com>
2025-04-30 23:00:44 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/20250430/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/20250430/keymap.c
@@ -165,7 +165,7 @@ void keyball_on_apply_motion_to_mouse_scroll(keyball_motion_t *m, report_mouse_t
        // 高速 -> 低いインデックス (1) -> 小さい除数 -> 速いスクロール
 
        // シグモイド関数の項 (速度に対して 0 から 1 へ変化, speed = 8.0 で 0.5)
-       float sigmoid_term = 1.0f / (1.0f + expf(-0.5f * (speed - 20.0f)));
+       float sigmoid_term = 1.0f / (1.0f + expf(-0.5f * (speed - 50.0f)));
 
        // sigmoid_term (0..1) を 除数インデックスの範囲 (1..7) にマッピング
        // 高速側 (sigmoid_term が 1 に近い) を インデックス 1 に、
@@ -188,7 +188,7 @@ void keyball_on_apply_motion_to_mouse_scroll(keyball_motion_t *m, report_mouse_t
 
        // 計算された浮動小数点数のインデックスを整数に丸め、1から7の範囲にクランプ
        int dynamic_n = (int)roundf(dynamic_n_float);
-       dynamic_n = MAX(1, MIN(10, dynamic_n));
+       dynamic_n = MAX(1, MIN(7, dynamic_n));
 
        // 動的な除数を計算
        dynamic_div = 1 << (dynamic_n - 1);
